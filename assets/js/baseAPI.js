82d7159aff22e86d2e25b504cfcb12037f1432cf
lisa413 <1830234786@qq.com>
2022-07-07 20:51:06 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,24 @@ $.ajaxPrefilter(function (options) {
   // console.log(options.url)
   // 在正式发起请求之前，拼接真正的根路径
   options.url = 'http://www.liulongbin.top:3007' + options.url
+
+  // 统一为有权限的接口设置headers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || '',
+    }
+  }
+
+  // 全局挂载complete函数
+  // 不论请求成功还是失败，都会执行complete回到函数
+  options.complete = function (res) {
+    if (
+      //  res.responseJSON: 服务器响应回来的数据
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+  getUserInfo()
+
+  var layer = layui.layer
+  // 点击退出按钮的监听事件
+  $('.btnLogOut').on('click', function () {
+    // 弹出层
+    layer.confirm(
+      '确定退出登录?',
+      { icon: 3, title: '提示' },
+      function (index) {
+        // 1. 清空本地存储的token
+        localStorage.removeItem('token')
+        // 2. 跳转页面
+        location.href = '/login.html'
+
+        layer.close(index)
+      }
+    )
+  })
+})
+
+// 获取用户的信息
+function getUserInfo(params) {
+  $.ajax({
+    url: '/my/userinfo',
+    method: 'GET',
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || '',
+    // },
+    success: function (res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取信息失败')
+      }
+      // 渲染用户头像
+      renderAvatar(res.data)
+    },
+  })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+  // 1.渲染名称
+  var name = user.nickname || user.username
+  $('#welcome').html('欢迎：' + name)
+  // 2.渲染头像
+  if (user.ser_pic != null) {
+    $('.layui-nav-img').attr('src', user.ser_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+  }
+}
